44eeaa6311439430c9e6aafe09188d168cb32e5e
StunMasteR <162857412+StunMasteR@users.noreply.github.com>
2025-12-10 19:43:55 +0200

    dz

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import css from './Footer.module.css';
+import Link from 'next/link';
+
+const Footer = () => {
+  return(<footer className={css.footer}>
+  <div className={css.content}>
+    <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+    <div className={css.wrap}>
+      <p>Developer: Igor Sheleknov</p>
+<p>
+  Contact us:&ensp;
+  <Link href="mailto:support@notehub.app">support@notehub.app</Link>
+</p>
+
+    </div>
+  </div>
+</footer>);
+}
+
+export default Footer;
